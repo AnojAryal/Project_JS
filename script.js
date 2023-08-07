225222771f9b1be168ae645fd4a8b0b8d3170052
FraNzY <anoj1810@gmail.com>
2023-08-07 21:33:46 +0545

    simple todo app

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-function greet() {
-    const name = prompt('What is your name?');
-    alert(`Hello, ${name}! Welcome to My Project!`);
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const li = document.createElement('li');
+        li.innerText = taskText;
+
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'Remove';
+        removeButton.onclick = function () {
+            taskList.removeChild(li);
+        };
+
+        li.appendChild(removeButton);
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
 }
 
+document.getElementById('taskInput').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
 
-window.onload = function() {
-    greet();
-}
+document.getElementById('addTaskButton').addEventListener('click', addTask);
